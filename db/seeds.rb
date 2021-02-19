64d6641994889d691c1cdfe5ddbc709246918ef7
Naomi <naomi.paulmin@gmail.com>
2021-02-19 16:25:36 +0100

    seed fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Movie.destroy_all
+20.times do
+  new_movie = Movie.new({ name: Faker::Movie.title, rating: rand(1..10) })
+  new_movie.save
+end
